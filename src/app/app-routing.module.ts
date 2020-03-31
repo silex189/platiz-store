d3edc4dc1cba3d2a29c470175e2629121665f2a9
silex189 <antoniojm89@gmail.com>
2020-03-31 23:40:42 +0200

    material schematics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,6 +32,9 @@ const routes: Routes = [
     ]
   },
   {
+    path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+  {
     path: '**',
     loadChildren: () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)
   }
