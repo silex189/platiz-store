57a8e990a436c976d3ce58b62d3e4323c1d3e805
silex189 <antoniojm89@gmail.com>
2020-04-05 23:44:45 +0200

    forms observables

--- src/app/admin/components/product-edit/product-edit.component.ts
+++ src/app/admin/components/product-edit/product-edit.component.ts
@@ -1,45 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { MyValidators } from '../../../utils/validators';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ProductsService } from 'src/app/core/services/products/products.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { MyValidators } from 'src/app/utils/validators';
 
 @Component({
-  selector: 'app-form-product',
-  templateUrl: './form-product.component.html',
-  styleUrls: ['./form-product.component.scss']
+  selector: 'app-product-edit',
+  templateUrl: './product-edit.component.html',
+  styleUrls: ['./product-edit.component.scss']
 })
-export class FormProductComponent implements OnInit {
+export class ProductEditComponent implements OnInit {
 
   form: FormGroup;
+  id: string;
 
   constructor(
     private formBuilder: FormBuilder,
     private productservice: ProductsService,
-    private router: Router
+    private router: Router,
+    private activeRoute: ActivatedRoute
   ) {
     this.buildForm();
   }
 
   ngOnInit(): void {
+    this.activeRoute.params.subscribe((params: Params) => {
+      this.id = params.id;
+      this.productservice.getProduct(this.id).subscribe(product => {
+        this.form.patchValue(product);
+      });
+    });
   }
 
   saveProduct(event: Event) {
     event.preventDefault();
     if (this.form.valid) {
       const product = this.form.value;
-      this.productservice.createProduct(product)
+      this.productservice.updateProduct(this.id, product)
         .subscribe( (newProduct) => {
         console.log(newProduct);
         this.router.navigate(['./admin/products'])
-      })
+      });
     }
   }
 
   private buildForm() {
     this.form = this.formBuilder.group({
-      id: ['', [Validators.required]],
       title: ['', [Validators.required]],
       price: ['', [Validators.required, MyValidators.isPriceValid]],
       image: '',
