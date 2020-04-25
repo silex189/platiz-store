c1321631b4bc717148aae10fa282811d1faaadaa
silex189 <antoniojm89@gmail.com>
2020-04-25 11:10:54 +0200

    environment modified

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,15 +6,15 @@ export const environment = {
   production: false,
   url_api: 'http://platzi-store.herokuapp.com',
   firebase: {
-    apiKey: 'AIzaSyArLRPyZ8a1MjBnDd5cK_FFcGiGfhzZTI4',
-    authDomain: 'platzi-store-dfe53.firebaseapp.com',
-    databaseURL: 'https://platzi-store-dfe53.firebaseio.com',
-    projectId: 'platzi-store-dfe53',
-    storageBucket: 'platzi-store-dfe53.appspot.com',
-    messagingSenderId: '114052267790',
-    appId: '1:114052267790:web:dc1bd5b7adca4f20c3e15f',
-    measurementId: 'G-RXC87ZG29G'
-  }
+    apiKey: "AIzaSyArLRPyZ8a1MjBnDd5cK_FFcGiGfhzZTI4",
+    authDomain: "platzi-store-dfe53.firebaseapp.com",
+    databaseURL: "https://platzi-store-dfe53.firebaseio.com",
+    projectId: "platzi-store-dfe53",
+    storageBucket: "platzi-store-dfe53.appspot.com",
+    messagingSenderId: "114052267790",
+    appId: "1:114052267790:web:dc1bd5b7adca4f20c3e15f",
+    measurementId: "G-RXC87ZG29G",
+  },
 };
 
 /*
